9302670793eca40c40410bb1a35d5e6947a54660
AdrielleDS <adrielleduttra@gmail.com>
2025-07-02 16:44:06 -0300

    Update primo.cpp

--- primo.cpp
+++ primo.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     int numero;
-    std::cout << "Digite um numero inteiro: ";
-    std::cin >> numero;
+    
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
 
     if (numero <= 1) {
-        std::cout << "Nao e primo" << std::endl;
+        cout << "Nao e primo" <<endl;
         return 0;
     }
 
     for (int i = 2; i * i <= numero; ++i) {
         if (numero % i == 0) {
-            std::cout << "Nao e primo" << std::endl;
+            
+            cout << "Nao e primo" << endl;
             return 0;
         }
     }
 
-    std::cout << "Primo" << std::endl;
+    cout << "Primo" << endl;
     return 0;
 }
